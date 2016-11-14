b15ae9fef0b56d85e1817a7ac3519fb6e44fdc60
vdemedes <vdemedes@gmail.com>
2016-11-14 22:51:24 +0200

    simplify code even more, thanks @sindresorhus!

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
 module.exports = (str, spaces) => {
-	const regex = new RegExp(' '.repeat(spaces || 2), 'g');
-
-	return str.replace(/^\s+/gm, $1 => $1.replace(regex, '\t'));
+	return str.replace(/^\s+/gm, $1 => '\t'.repeat($1.length / (spaces || 2)));
 };
